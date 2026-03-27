89d1363a1134d7c0c11009cf2e47378fbc0b1e2b
CF8339816 <chris_french_3dga@hotmail.com>
2026-03-27 14:55:24 -0300

    adding logic working on arraay

--- Prog2_Chall_C_StackableItems_ChrisFrench0259182/Inventory.cs
+++ Prog2_Chall_C_StackableItems_ChrisFrench0259182/Inventory.cs
@@ -12,41 +12,56 @@ namespace Prog2_Chall_C_StackableItems_ChrisFrench0259182
               
         public InventorySlot[] _items;
         private int _size { get;  set; }
-      
-        protected Inventory(int Size)
-        {
+
+        public Inventory(int Size)
+        { _size = Size;
+            //_items[i] = new InventorySlot[Size];   
+
             Size = 4;
-            _size = Size;
-            _items = new InventorySlot[Size];   
-           
+
+
+            for (int i = 0; i < Size; i++)
+            {
+
+                // _items[i] = item;
+                _items[i] = new InventorySlot();
+
+
+
+            }
         }
 
-       
+
 
 
 
         public void AddItem()
-        {          
-           
+        {
+
             Console.ReadKey();
 
 
 
-            switch(Item item)
+            switch (Item item)
             {
-                case 1: item = Coin.coin;break;
-                case 2: item = RestorePotion;break;
+                case 1: item = Coin.coin; break;
+                case 2: item = RestorePotion; break;
                 case 3: item = WizardHat; break;
                 case 4: item = Dagger.dagger; break;
-                
-            }
+
+                }
+
+                //for (int i = 0; i < Size; i++)
+                //{
+
+                //    _items[i] = item;
+                //}
 
 
-            for (int i = 0; i < _items.Length; i++)
-            {
+            } 
+        }
 
-                _items[i] = item;
-             
+         
 
         public void RemoveItem()
         {
